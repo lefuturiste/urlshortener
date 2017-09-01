a9f37d8dbbd2fd63d0038be5300f00181f396127
Matthieu Bessat <mail@matthieubessat.fr>
2017-09-01 21:04:50 +0200

    Fix auth & fault

--- App/Middlewares/Authorisation.php
+++ App/Middlewares/Authorisation.php
@@ -22,13 +22,13 @@ class Authorisation
 	public function __invoke($request, $response, $next)
 	{
 		if (isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) {
-			if (array_search($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW'], $this->container->config['api_auth']['auth_data']) !== false) {
+			if ($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW'] == $this->container->config['api_auth']) {
 				return $next($request, $response);
 			} else {
 				return $response->write('Error with authentification : incorect authentification')->withStatus(401);
 			}
 		} else {
-			return $response->write('Error with authentification : no authentification provided')->withSatus(401);
+			return $response->write('Error with authentification : no authentification provided')->withStatus(401);
 		}
 	}
 }
